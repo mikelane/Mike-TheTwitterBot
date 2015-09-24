bfa71fd2cad72a16d50515a81252200b8b905f21
Michael Lane <mikelane@gmail.com>
2015-09-23 18:46:36 -0700

    cosmetic changes.

--- TwitterBot.py
+++ TwitterBot.py
@@ -14,13 +14,20 @@ __email__ = "mikelane@gmail.com"
 
 import os
 from TwitterAPI import TwitterAPI
+import nltk
+
 
 consumer_key = os.environ["MTTB_API_KEY"]
 consumer_secret = os.environ["MTTB_API_SECRET"]
 access_token_key = os.environ["MTTB_ACCESS_TOKEN"]
 access_token_secret = os.environ["MTTB_ACCESS_TOKEN_SECRET"]
 
-api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
+api = TwitterAPI(
+    consumer_key,
+    consumer_secret,
+    access_token_key,
+    access_token_secret
+)
 
 # r = api.request('statuses/update', {'status':'Tweeting from python!'})
 # print(r.text)
